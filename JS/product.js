313bce2dfba5fe593ec5f9400d6feb960b910c64
Mathieu Botwin <mathieu.btw@gmail.com>
2021-09-26 11:35:09 +0200

    cookie recup

---
+++ JS/product.js
@@ -0,0 +1,36 @@
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + ";" + expires + ";";
+  }
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+
+  function deleteCookie(name) { setCookie(name, '', -1); }
+
+let productid = getCookie('product_choose');
+
+fetch("http://localhost:3000/api/teddies/"+ productid)
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
