0fce05a8d6b3fc70df8d71ab02b347ad2bbfd675
Mathieu Botwin <mathieu.btw@gmail.com>
2021-10-02 13:21:06 +0200

    1.7

--- JS/basket.js
+++ JS/basket.js
@@ -1,5 +1,4 @@
-// let shoppingBasket = JSON.parse(getCookie('shoppingBasket'));
-let shoppingBasket = [{"colors":["Pale brown","Dark brown","White"],"_id":"5beaa8bf1c9d440000a57d94","name":"Arnold","price":3900,"imageUrl":"http://localhost:3000/images/teddy_2.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"colors":["Tan","Chocolate","Black","White"],"_id":"5be9c8541c9d440000665243","name":"Norbert","price":2900,"imageUrl":"http://localhost:3000/images/teddy_1.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"colors":["Brown"],"_id":"5beaaa8f1c9d440000a57d95","name":"Lenny and Carl","price":5900,"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","imageUrl":"http://localhost:3000/images/teddy_3.jpg"}];
+let shoppingBasket = JSON.parse(getCookie('shoppingBasket'));
 let totalBasket = 0;
 for (let i = 0; i < shoppingBasket.length; i++) {
  
