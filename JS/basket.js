14529c2bf62f00e372b72e5af1ba711d2bf393ce
Mathieu Botwin <mathieu.btw@gmail.com>
2021-10-02 13:19:11 +0200

    1.7

--- JS/basket.js
+++ JS/basket.js
@@ -1,4 +1,5 @@
-let shoppingBasket = JSON.parse(getCookie('shoppingBasket'));
+// let shoppingBasket = JSON.parse(getCookie('shoppingBasket'));
+let shoppingBasket = [{"colors":["Pale brown","Dark brown","White"],"_id":"5beaa8bf1c9d440000a57d94","name":"Arnold","price":3900,"imageUrl":"http://localhost:3000/images/teddy_2.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"colors":["Tan","Chocolate","Black","White"],"_id":"5be9c8541c9d440000665243","name":"Norbert","price":2900,"imageUrl":"http://localhost:3000/images/teddy_1.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"colors":["Brown"],"_id":"5beaaa8f1c9d440000a57d95","name":"Lenny and Carl","price":5900,"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","imageUrl":"http://localhost:3000/images/teddy_3.jpg"}];
 let totalBasket = 0;
 for (let i = 0; i < shoppingBasket.length; i++) {
  
@@ -11,7 +12,6 @@ for (let i = 0; i < shoppingBasket.length; i++) {
     newImg.id = 'productImg'+i; 
     newImg.classList.add("img-fluid","rounded-start");
     newImg.src=(shoppingBasket[i].imageUrl);
-    console.log=(shoppingBasket[i].imageUrl);
     document.getElementById("productCol"+i).appendChild(newImg);
 
     let newCol2 = document.createElement("div"); 
@@ -49,11 +49,10 @@ for (let i = 0; i < shoppingBasket.length; i++) {
     newButton.classList.add("btn", "btn-danger");
     newButton.textContent = "Supprimer le produit";
     document.getElementById("productBody"+i).appendChild(newButton);  
-    console.log(shoppingBasket);
     let buttonClick = document.getElementById("buttonid"+i);   
-      buttonClick.addEventListener('click', function() {  
+    buttonClick.addEventListener('click', function() {  
+        delete shoppingBasket[i];
         console.log(shoppingBasket);
-       
-      });
+    });
 
 }
