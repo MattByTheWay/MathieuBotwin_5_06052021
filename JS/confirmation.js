6a3b294c159762d07e82430542fb32d03c87cd55
Mathieu Botwin <mathieu.btw@gmail.com>
2021-10-07 21:02:55 +0200

    confirmationi

--- JS/confirmation.js
+++ JS/confirmation.js
@@ -1,4 +1,7 @@
 let confirmationOrder = JSON.parse(getCookie('orderConfirmation'));
+let totalPayout = 0;
 console.log(confirmationOrder);
 
-document.getElementById("commandID").innerHTML = '';
+document.getElementById("commandID").innerHTML = 'N°' + confirmationOrder.orderId;
+
+document.getElementById("totalPayout").innerHTML = 'Prix total :' + totalPayout;
