da2f55c5c58a92a2327f2100601cf6f8a639f6f7
Mathieu Botwin <mathieu.btw@gmail.com>
2021-10-07 20:47:11 +0200

    Confirmation

--- JS/confirmation.js
+++ JS/confirmation.js
@@ -1,5 +1,4 @@
 let confirmationOrder = JSON.parse(getCookie('orderConfirmation'));
 console.log(confirmationOrder);
-deleteCookie('orderConfirmation');
 
 document.getElementById("commandID").innerHTML = '';
